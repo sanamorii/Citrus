46e9e84297d970a53b723f89992d5b1f55fbbbb5
Jian <86570135+sanamorii@users.noreply.github.com>
2023-02-25 16:24:25 +0000

    inital ecs

--- include/citrus_core/entity.h
+++ include/citrus_core/entity.h
@@ -1,12 +1,28 @@
 #ifndef CITRUSENTITY_H
 #define CITRUSENTITY_H
 
-enum EntityTypes{
-    NONE,
-    PLAYER,
-    NPC,
-    PROP
+#include "citrus_core/core.h"
+#include "citrus_utils/linear.h"
+
+enum EntityComponents{
+    POSITION,
+    PHYSICS,
+    SPRITE,
+    AUDIO
 };
+
+enum SpriteType{
+    Static,
+    Animated
+};
+
+enum SpriteSheetIndex{
+    IDLE,
+    WALK,
+    RUN,
+    ATTACK
+};
+
 
 typedef struct Entity{
     long int id;
@@ -14,7 +30,33 @@ typedef struct Entity{
 
     void *sprite;
 
+    void **components;
+    int component_num;
+
 }Entity;
+
+typedef Vector C_Position;
+
+typedef struct C_Sprite{
+    void *sprite;
+    int type;
+}C_Sprite;
+
+typedef struct StaticSprite{
+    SDL_Texture *texture;
+}StaticSprite;
+
+typedef struct AnimatedSprite{
+    SDL_Texture ***sheet;  // a pointer to an array of pointers to another array of pointers
+    int *frame_num;
+    double interval;
+}AnimatedSprite;
+
+Entity *create_entity();
+void destroy_entity();
+
+int create_component(Entity *);
+void remove_component(Entity *, int component_type);
 
 
 #endif
